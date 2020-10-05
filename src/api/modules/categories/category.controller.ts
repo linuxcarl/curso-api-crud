966578c355d093196d61f74be99f2d6345827103
Carlos Ramirez <carlos@extrasistemas.com>
2020-10-04 20:28:58 -0500

    implements sample with injection of service

--- src/api/modules/categories/category.controller.ts
+++ src/api/modules/categories/category.controller.ts
@@ -1,12 +1,15 @@
 import { GET, route } from 'awilix-express';
 import { Request, Response } from 'express';
+import { CategoryService } from './category.service';
 
 @route('/categories')
 export class CategoryController {
+  public constructor(private readonly categoryService: CategoryService) {}
   @GET()
   public index(req: Request, res: Response): void {
+    const result: any = this.categoryService.index();
     res.send({
-      controller: 'Category controller'
+      saludo: result
     });
   }
 }
