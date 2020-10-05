c090942f8498d80c53776b20fb738014359c2177
Carlos Ramirez <carlos@extrasistemas.com>
2020-10-05 14:51:33 -0500

    added mocks by category

---
+++ src/commons/store/mocks.ts
@@ -0,0 +1,86 @@
+const db = {
+  categories: [
+    {
+      id: 1,
+      title: 'Lacteos',
+      active: 1,
+      deleted: 1,
+      created_at: '2020-09-27',
+      updated_at: '2020-06-19'
+    },
+    {
+      id: 2,
+      title: 'Vinos y licores',
+      active: 0,
+      deleted: 1,
+      created_at: '2020-08-15',
+      updated_at: '2020-09-10'
+    },
+    {
+      id: 3,
+      title: 'Panaderia',
+      active: 0,
+      deleted: 1,
+      created_at: '2019-11-22',
+      updated_at: '2020-02-10'
+    },
+    {
+      id: 4,
+      title: 'Frutas y verduras',
+      active: 0,
+      deleted: 0,
+      created_at: '2020-08-31',
+      updated_at: '2019-12-29'
+    },
+    {
+      id: 5,
+      title: 'Carnes',
+      active: 1,
+      deleted: 1,
+      created_at: '2020-01-09',
+      updated_at: '2020-04-08'
+    },
+    {
+      id: 6,
+      title: 'Congelados',
+      active: 0,
+      deleted: 1,
+      created_at: '2020-03-01',
+      updated_at: '2020-01-08'
+    },
+    {
+      id: 7,
+      title: 'Bebidas',
+      active: 1,
+      deleted: 1,
+      created_at: '2019-12-29',
+      updated_at: '2020-04-12'
+    },
+    {
+      id: 8,
+      title: 'Ropa',
+      active: 1,
+      deleted: 1,
+      created_at: '2020-08-06',
+      updated_at: '2020-09-05'
+    },
+    {
+      id: 9,
+      title: 'Ferreteria',
+      active: 1,
+      deleted: 1,
+      created_at: '2020-06-04',
+      updated_at: '2020-03-12'
+    },
+    {
+      id: 10,
+      title: 'Papeleria',
+      active: 0,
+      deleted: 1,
+      created_at: '2020-05-18',
+      updated_at: '2019-10-19'
+    }
+  ]
+};
+
+export default db;
